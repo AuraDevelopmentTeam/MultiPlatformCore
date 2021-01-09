5256002d9de9212bd937a374b16e7aafb2b4c225
BrainStone <yannick@tedworld.de>
2021-01-09 06:34:15 +0100

    Added more test cases for SLF4J code

--- src/test/java/team/aura_dev/lib/multiplatformcore/MultiProjectSLF4JBootstrapperTest.java
+++ src/test/java/team/aura_dev/lib/multiplatformcore/MultiProjectSLF4JBootstrapperTest.java
@@ -2,8 +2,11 @@ package team.aura_dev.lib.multiplatformcore;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import org.junit.Rule;
@@ -13,6 +16,8 @@ import team.aura_dev.lib.multiplatformcore.testcode.slf4j.TestBootstrapper;
 import team.aura_dev.lib.multiplatformcore.testcode.slf4j.TestPluginBootstrap;
 
 public class MultiProjectSLF4JBootstrapperTest {
+  private static final String SLF4J_VERSION = "1.7.25";
+
   @Rule public TemporaryFolder folder = new TemporaryFolder();
 
   @Test
@@ -62,5 +67,50 @@ public class MultiProjectSLF4JBootstrapperTest {
 
     // Just calling with no feedback to make sure no exceptions
     plugin.testCall();
+  }
+
+  @Test
+  public void manualLoadTest() throws IOException {
+    final MultiProjectSLF4JBootstrapper<Object> bootstrapper =
+        new MultiProjectSLF4JBootstrapper<Object>(Object.class) {};
+    final Path libsDir = folder.newFolder("libsDir").toPath();
+
+    bootstrapper.extractAndInjectSLF4JLib(libsDir, SLF4J_VERSION, "api");
+
+    URL[] urls = bootstrapper.getDependencyClassLoader().getURLs();
+
+    assertEquals(2, urls.length);
+    assertUrlIsOk(urls[1]);
+  }
+
+  @Test
+  public void manualDoubleLoadTest() throws IOException {
+    final MultiProjectSLF4JBootstrapper<Object> bootstrapper =
+        new MultiProjectSLF4JBootstrapper<Object>(Object.class) {};
+    final Path libsDir = folder.newFolder("libsDir").toPath();
+
+    bootstrapper.extractAndInjectSLF4JLib(libsDir, SLF4J_VERSION, "api");
+    bootstrapper.extractAndInjectSLF4JLib(libsDir, SLF4J_VERSION, "api");
+
+    URL[] urls = bootstrapper.getDependencyClassLoader().getURLs();
+
+    // Will not get added twice
+    assertEquals(2, urls.length);
+    assertUrlIsOk(urls[1]);
+  }
+
+  private static void assertUrlIsOk(URL url) {
+    final String urlStr = url.toString();
+
+    assertTrue(
+        "expected URL to start with <file:/> but was:<" + urlStr + ">",
+        urlStr.startsWith("file:/"));
+    assertTrue(
+        "expected URL to end with <libsDir/org/slf4j/api/slf4j-api-"
+            + SLF4J_VERSION
+            + ".jar> but was:<"
+            + urlStr
+            + ">",
+        urlStr.endsWith("libsDir/org/slf4j/api/slf4j-api-" + SLF4J_VERSION + ".jar"));
   }
 }
