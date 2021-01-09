7009823e9f8366d8c2e73962f6a7349350ad96bd
BrainStone <yannick@tedworld.de>
2021-01-09 01:46:13 +0100

    Forgot to remove debugging code

--- src/test/java/team/aura_dev/lib/multiplatformcore/testcode/BootstrapTest.java
+++ src/test/java/team/aura_dev/lib/multiplatformcore/testcode/BootstrapTest.java
@@ -1,13 +1,6 @@
 package team.aura_dev.lib.multiplatformcore.testcode;
 
-import org.junit.Assert;
-import org.junit.Test;
 import team.aura_dev.lib.multiplatformcore.MultiProjectBootstrap;
 import team.aura_dev.lib.multiplatformcore.testcode.api.TestPluginApi;
 
-public class BootstrapTest extends MultiProjectBootstrap<TestPluginApi> {
-  @Test
-  public void test() {
-    Assert.fail();
-  }
-}
+public class BootstrapTest extends MultiProjectBootstrap<TestPluginApi> {}
